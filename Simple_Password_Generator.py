02876d4482e23a8b0438e412cd4aa739077937c7
Abdulk786 <abdulkadirjsadan@gmail.com>
2025-04-29 20:41:42 +0530

    3rd Commit

--- Simple_Password_Generator.py
+++ Simple_Password_Generator.py
@@ -4,6 +4,7 @@ import random
 from tkinter import *
 from tkinter import BooleanVar
 
+
 import ttkbootstrap as tb
 
 #Setup
@@ -15,7 +16,7 @@ root.grid_columnconfigure(0, weight=1)  # Allow column to expand
 
 
 #Initialization
-len_var = tb.IntVar(root, '')
+len_var = tb.StringVar(root, '')
 number_tog=BooleanVar(root, False)
 char_tog=BooleanVar(root, False)
 sp_char_tog=BooleanVar(root,False)
@@ -29,25 +30,45 @@ while(i<=20):
     i+=1
 
 def generate():
-    len_var_value=len_var.get()
+    #Error Handling
+    if not(len_var.get().isdigit()) or (int(len_var.get()) < 5) or (int(len_var.get())>20):
+        label_result.config(text='Error : Invalid Lenght of Password', bootstyle='danger')
+        return
+    elif(number_tog.get() == False and char_tog.get() == False and sp_char_tog.get() == False):
+        label_result.config(text='Error : Select atlest 1 of 3 options', bootstyle='danger')
+        return
+    else:
+        label_result.config(text='')
+        pass
+
+    len_var_value=int(len_var.get())
     number_tog_value=number_tog.get()
     char_tog_value=char_tog.get()
     sp_char_tog_value=sp_char_tog.get()
+    all_char = []
+
     print(number_tog_value)
     print(char_tog_value)
     print(sp_char_tog_value)
     print(len_var_value)
-    if(number_tog_value==TRUE and char_tog_value==FALSE and sp_char_tog_value==FALSE):
-        all_char=[set_int]
-        result=[]
-        j=0
-        while(j<len_var_value):
-            result.append(random.choice(set_int))
-            j+=1
-    elif(number_tog_value==TRUE and char_tog_value==FALSE and sp_char_tog_value==FALSE)
+
+    if(number_tog_value==1):
+        all_char.extend(set_int)
+    if(char_tog_value==1):
+        all_char.extend(set_char)
+        all_char.extend(item.upper() for item in set_char)
+    if (sp_char_tog_value==1):
+        all_char.extend(set_symbol)
+    random.shuffle(all_char)
+    print(all_char)
+    result=[]
+    j=0
+    while(j<len_var_value):
+        result.append(random.choice(all_char))
+        j+=1
     result_joined = "".join(str(item) for item in result)
-    label_result = tb.Label(root, text=result_joined, font=("Helvetica", 10, "bold"))
-    label_result.grid(row=6, column=0, pady=10)
+    label_result.config(text="")
+    label_result.config(text=result_joined, bootstyle='primary')
 
 #Heading
 heading = tb.Label(root, text="Simple Password Generator", font=("Helvetica", 17, "bold"), foreground="navy")
@@ -75,5 +96,8 @@ btn_sp_char.grid(row=4, column=0, padx=20, pady=10, sticky='w')
 btn_sub=tb.Button(root, text='Enter', command=generate)
 btn_sub.grid(row=5, column=0,ipadx=10,ipady=5, padx=100, pady=20)
 
+#Result/Error Messeage Box
+label_result = tb.Label(root, text='', font=("Helvetica", 10, "bold"))
+label_result.grid(row=6, column=0, pady=10)
 
 root.mainloop()
